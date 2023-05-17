0bcbd953f7d1f1576d5fc6b5029e2ed198d5f987
unknown <sandarurashmitha98@gmail.com>
2023-05-17 16:59:01 +0530

    Conntected to OpenAi Api

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,13 +7,33 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-    apiKey: process.env.OpenAI_API_KEY,
+    apiKey: process.env.OPEN_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-    res.send("Hello From dalle-e");
+    res.status(200).json({ message: "Hello from DALL-E!" });
+});
+
+router.route("/").post(async(req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: "1024x1024",
+            response_format: "b64_json",
+        });
+
+        const image = aiResponse.data.data[0].b64_json;
+        res.status(200).json({ photo: image });
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+        //  .send(error ? .response.data.error.message || "Something went wrong");
+    }
 });
 
 export default router;
